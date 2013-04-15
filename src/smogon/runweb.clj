00f21dacc897328a732064f1e90211362e1a907d
Christopher Monsanto <chris@monsan.to>
2013-04-15 18:51:10 -0400

    Clean up runweb imports; add debug mode support.

--- src/smogon/runweb.clj
+++ src/smogon/runweb.clj
@@ -1,22 +1,27 @@
 
 (ns smogon.runweb
-  (:require [org.httpkit server]
-            [compojure route handler]
-            [compojure.core :refer :all]
-            [ring.middleware.stacktrace :refer :all]
-            [taoensso.timbre :as log]))
+  (:use org.httpkit.server
+        (compojure core route handler)
+        (ring.middleware stacktrace reload))
+  (:require [taoensso.timbre :as log]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (compojure.route/not-found "Not Found"))
 
+(def app (-> app-routes site wrap-stacktrace-log))
+(def port 9001)
+
 (defn runweb
   "Run the server."
   []
-  (let [app (-> app-routes
-                compojure.handler/site
-                wrap-stacktrace-log)
-        port 9001]
-    (org.httpkit.server/run-server app {:port port})
-    (log/info "Starting Smogon web server on port" port)))
+  (run-server app {:port port})
+  (log/info "Starting Smogon web server on port" port))
 
+(defn runweb-dev
+  "Run the server."
+  []
+  ;; The #' lets us dynamically update app-routes.
+  (let [app-dev (-> #'app wrap-reload)] 
+    (run-server app-dev {:port port})
+    (log/info "Starting Smogon web server (DEV!!!) on port" port)))
