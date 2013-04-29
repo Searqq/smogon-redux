c6dfee1e0c6f8cf779f65bfec10f8bb3da53df98
tummychow <sforstephen@hotmail.com>
2013-04-29 00:42:29 -0300

    heatran dpphgss eruption event
    added to bw as well

--- src/pokedb/pokemon/heatran.clj
+++ src/pokedb/pokemon/heatran.clj
@@ -1,4 +1,3 @@
-
 (defpokemon :heatran
   :name "Heatran"
   :introduced-in :dp
@@ -12,7 +11,7 @@
 (deffamily :heatran)
 
 (deflearnset :dp
-  :heatran [:ancientpower :attract :bug-bite :captivate :crunch :dark-pulse :dig :double-team :dragon-pulse :earth-power :earthquake :endure :explosion :facade :fire-blast :fire-fang :fire-spin :flamethrower :flash-cannon :frustration :giga-impact :headbutt :heat-wave :hidden-power :hyper-beam :iron-defense :iron-head :lava-plume :leer :magma-storm :metal-sound :mud-slap :natural-gift :overheat :payback :protect :rest :return :roar :rock-climb :rock-slide :rock-smash :rock-tomb :scary-face :secret-power :sleep-talk :snore :solarbeam :stealth-rock :stone-edge :strength :substitute :sunny-day :swagger :taunt :torment :toxic :uproar :will-o-wisp])
+  :heatran [:ancientpower :attract :bug-bite :captivate :crunch :dark-pulse :dig :double-team :dragon-pulse :earth-power :earthquake :endure :explosion :facade :fire-blast :fire-fang :fire-spin :flamethrower :flash-cannon :frustration :giga-impact :headbutt :heat-wave :hidden-power :hyper-beam :iron-defense :iron-head :lava-plume :leer :magma-storm :metal-sound :mud-slap :natural-gift :overheat :payback :protect :rest :return :roar :rock-climb :rock-slide :rock-smash :rock-tomb :scary-face :secret-power :sleep-talk :snore :solarbeam :stealth-rock :stone-edge :strength :substitute :sunny-day :swagger :taunt :torment :toxic :uproar :will-o-wisp :eruption])
 
 (deflearnset :bw
-  :heatran [:ancientpower :attract :bug-bite :bulldoze :crunch :dark-pulse :dig :double-team :dragon-pulse :earth-power :earthquake :explosion :facade :fire-blast :fire-fang :fire-spin :flame-charge :flamethrower :flash-cannon :frustration :giga-impact :heat-wave :hidden-power :hyper-beam :incinerate :iron-defense :iron-head :lava-plume :leer :magma-storm :metal-sound :overheat :payback :protect :rest :return :roar :rock-slide :rock-smash :rock-tomb :round :scary-face :sleep-talk :snore :solarbeam :stealth-rock :stone-edge :strength :substitute :sunny-day :swagger :taunt :torment :toxic :uproar :will-o-wisp])
+  :heatran [:ancientpower :attract :bug-bite :bulldoze :crunch :dark-pulse :dig :double-team :dragon-pulse :earth-power :earthquake :explosion :facade :fire-blast :fire-fang :fire-spin :flame-charge :flamethrower :flash-cannon :frustration :giga-impact :heat-wave :hidden-power :hyper-beam :incinerate :iron-defense :iron-head :lava-plume :leer :magma-storm :metal-sound :overheat :payback :protect :rest :return :roar :rock-slide :rock-smash :rock-tomb :round :scary-face :sleep-talk :snore :solarbeam :stealth-rock :stone-edge :strength :substitute :sunny-day :swagger :taunt :torment :toxic :uproar :will-o-wisp :eruption])
