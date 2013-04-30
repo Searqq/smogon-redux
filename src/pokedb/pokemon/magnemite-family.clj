bb92fc788435c7b2da47a581b799142154398881
tummychow <sforstephen@hotmail.com>
2013-04-29 22:04:29 -0300

    nypc agility magnemite, XD refresh magneton/zone

--- src/pokedb/pokemon/magnemite-family.clj
+++ src/pokedb/pokemon/magnemite-family.clj
@@ -1,4 +1,3 @@
-
 (defpokemon :magnemite
   :name "Magnemite"
   :introduced-in :rb
@@ -36,23 +35,23 @@
 (deffamily :magnemite :magneton :magnezone)
 
 (deflearnset :rb
-  :magnemite [:bide :double-edge :double-team :flash :mimic :rage :reflect :rest :screech :sonicboom :substitute :supersonic :swift :tackle :take-down :teleport :thunder :thunder-wave :thunderbolt :thundershock :toxic]
+  :magnemite [:bide :double-edge :double-team :flash :mimic :rage :reflect :rest :screech :sonicboom :substitute :supersonic :swift :tackle :take-down :teleport :thunder :thunder-wave :thunderbolt :thundershock :toxic :agility]
   :magneton [:hyper-beam])
 
 (deflearnset :gs
-  :magnemite [:curse :double-team :endure :flash :frustration :hidden-power :lock-on :protect :rain-dance :rest :return :rollout :screech :sleep-talk :snore :sonicboom :supersonic :swagger :swift :tackle :thunder :thunder-wave :thunderbolt :thundershock :toxic :zap-cannon]
+  :magnemite [:curse :double-team :endure :flash :frustration :hidden-power :lock-on :protect :rain-dance :rest :return :rollout :screech :sleep-talk :snore :sonicboom :supersonic :swagger :swift :tackle :thunder :thunder-wave :thunderbolt :thundershock :toxic :zap-cannon :agility]
   :magneton [:hyper-beam :tri-attack])
 
 (deflearnset :rs
   :magnemite [:double-edge :double-team :endure :facade :flash :frustration :hidden-power :lock-on :metal-sound :mimic :protect :rain-dance :reflect :rest :return :rollout :screech :secret-power :shock-wave :sleep-talk :snore :sonicboom :spark :substitute :sunny-day :supersonic :swagger :swift :tackle :thunder :thunder-wave :thunderbolt :thundershock :toxic :zap-cannon]
-  :magneton [:hyper-beam :tri-attack])
+  :magneton [:hyper-beam :tri-attack :refresh])
 
 (deflearnset :dp
   :magnemite [:charge-beam :discharge :double-team :endure :explosion :facade :flash :flash-cannon :frustration :gravity :gyro-ball :hidden-power :iron-defense :light-screen :lock-on :magic-coat :magnet-bomb :magnet-rise :metal-sound :mirror-shot :natural-gift :protect :psych-up :rain-dance :recycle :reflect :rest :return :rollout :screech :secret-power :shock-wave :signal-beam :sleep-talk :snore :sonicboom :spark :substitute :sunny-day :supersonic :swagger :swift :tackle :thunder :thunder-wave :thunderbolt :thundershock :toxic :zap-cannon]
-  :magneton [:giga-impact :hyper-beam :tri-attack]
+  :magneton [:giga-impact :hyper-beam :tri-attack :refresh]
   :magnezone [:barrier :iron-head :mirror-coat])
 
 (deflearnset :bw
   :magnemite [:charge-beam :discharge :double-team :electro-ball :electroweb :explosion :facade :flash :flash-cannon :frustration :gravity :gyro-ball :hidden-power :iron-defense :light-screen :lock-on :magic-coat :magnet-bomb :magnet-rise :metal-sound :mirror-shot :protect :psych-up :rain-dance :recycle :reflect :rest :return :round :screech :signal-beam :sleep-talk :snore :sonicboom :spark :substitute :sunny-day :supersonic :swagger :tackle :thunder :thunder-wave :thunderbolt :thundershock :toxic :volt-switch :wild-charge :zap-cannon]
-  :magneton [:giga-impact :hyper-beam :tri-attack]
+  :magneton [:giga-impact :hyper-beam :tri-attack :refresh]
   :magnezone [:barrier :iron-head :mirror-coat])
